62e8612fb55f6fb7b2143aa011b84fb6430abf1b
Ben Woosley <ben.woosley@gmail.com>
2010-07-12 00:31:02 -0400

    NY: Add headshot_url to assembly members, extract UpperChamber and LowerChamber to serve.
    Note we're not scraping the image directly from the home page, rather just following their conventions.  This could be more robust with a check for the image's presence.

--- fiftystates/scrape/ny/legislators.py
+++ fiftystates/scrape/ny/legislators.py
@@ -9,17 +9,44 @@ votesmart.apikey = settings.VOTESMART_API_KEY
 class NYLegislatorScraper(LegislatorScraper):
     state = 'ny'
 
+    class UpperChamber:
+        def __init__(self):
+            self.name = 'upper'
+            self.vote_smart_office_id = 9
+
+        def home_page_for(self, official):
+            return ""
+
+        def scrape_home_page_for(self, official):
+            return {}
+
+    class LowerChamber:
+        def __init__(self):
+            self.name = 'lower'
+            self.vote_smart_office_id = 7
+            self.home_page_root = "http://assembly.state.ny.us/mem/"
+
+        def home_page_for(self, official):
+            return self.home_page_root + "?ad=" + official.officeDistrictName
+
+        def scrape_home_page_for(self, official):
+            return {"headshot_url": self.home_page_root + 'hdgimages/' + official.officeDistrictName.rjust(3, '0') + '_hdrhs.png'}
+
     def scrape(self, chamber, year):
         if chamber == 'upper':
-            officeId = 9
+            chamber = self.UpperChamber()
         else:
-            officeId = 7
-        for official in votesmart.officials.getByOfficeState(officeId, 'NY'):
+            chamber = self.LowerChamber()
+        for official in votesmart.officials.getByOfficeState(chamber.vote_smart_office_id, 'NY'):
             full_name = official.firstName
             if official.middleName:
                 full_name += " " + official.middleName
             full_name += " " + official.lastName
-            leg = Legislator('2009-2010', chamber, official.officeDistrictName,
+
+            leg = Legislator('2009-2010', chamber.name, official.officeDistrictName,
                              full_name, official.firstName, official.lastName,
-                             official.middleName, official.officeParties)
+                             official.middleName, official.officeParties,
+                             **chamber.scrape_home_page_for(official))
+            leg.add_source(chamber.home_page_for(official))
+
             self.save_legislator(leg)
